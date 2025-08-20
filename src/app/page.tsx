23cd36419d0159693161ecce7638f10b02d94097
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-08-20 22:04:03 +0900

    chore: buttonのimport テスト

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="text-red-500">aaaaaaaaa</div>
+    <div className="text-red-500">
+      <Button>Click me </Button>
+    </div>
   );
 }
