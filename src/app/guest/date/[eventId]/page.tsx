7b8ab73ada5fe13c7400824664cca236945f1d8c
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-08-23 04:01:19 +0900

    feat: 型定義

--- src/app/guest/date/[eventId]/page.tsx
+++ src/app/guest/date/[eventId]/page.tsx
@@ -1,4 +1,18 @@
 import React from 'react'
+type PossibleDate = {
+  id: number;
+  periodStart: string; // RFC3339準拠の日時文字列 (例: "2025-09-01T10:00:00Z")
+  periodEnd: string;
+  participateMemberNum: number;
+};
+
+type DateResult = {
+  eventName: string;
+  votedCount: number;
+  memo: string;
+  durationMin: number;
+  possibleDate: PossibleDate[];
+};
 
 const page = () => {
   return (
