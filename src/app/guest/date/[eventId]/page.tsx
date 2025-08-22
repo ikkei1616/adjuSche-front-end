764c3cb6d35bcd7cba8d69980fc2312277ed163f
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-08-23 04:02:08 +0900

    feat:mockデータの追加

--- src/app/guest/date/[eventId]/page.tsx
+++ src/app/guest/date/[eventId]/page.tsx
@@ -14,6 +14,27 @@ type DateResult = {
   possibleDate: PossibleDate[];
 };
 
+const mockEvents: DateResult = {
+  eventName: "サークル BBQ",
+  votedCount: 20,
+  memo: "参加費は1人500円です。",
+  durationMin: 120,
+  possibleDate: [
+    {
+      id: 1,
+      periodStart: "2025-09-01T10:00:00Z",
+      periodEnd: "2025-09-01T12:00:00Z",
+      participateMemberNum: 10,
+    },
+    {
+      id: 2,
+      periodStart: "2025-09-05T14:00:00Z",
+      periodEnd: "2025-09-05T16:00:00Z",
+      participateMemberNum: 7,
+    },
+  ],
+};
+
 const page = () => {
   return (
     <div>page</div>
