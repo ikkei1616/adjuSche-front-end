590a73cdc034a07ecb3a7972d32d0bbe877174fb
秋田優斗 <yuto@akitayuutonoMacBook-Air.local>
2025-08-22 23:10:42 +0900

    イベント時間の設定をするカードの仮作成を行いました

--- src/app/create-new-event/page.tsx
+++ src/app/create-new-event/page.tsx
@@ -1,12 +1,24 @@
+"use client";
 
+import { useState } from "react";
+import EventTimeCard, { EventTimeValue } from "@/components/ui/EventTimeCard";
 import EventNameCard from "@/components/ui/EventNameCard";
 
 export default function Home() {
-  return (
-    <div className="text-red-500">
-          <EventNameCard>
-          </EventNameCard>
-    </div>
+  const [time, setTime] = useState<EventTimeValue>({ option: "" });
 
+  return (
+    <div className="p-6 space-y-6">
+      <EventNameCard />
+
+      <EventTimeCard
+        value={time}
+        onChange={setTime}
+       
+      />
+
+      {/* デバッグ用（不要なら削除OK） */}
+      <pre className="text-xs opacity-70">{JSON.stringify(time, null, 2)}</pre>
+    </div>
   );
 }
