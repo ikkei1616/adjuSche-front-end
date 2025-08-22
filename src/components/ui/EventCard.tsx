ace3f7b2733998f984b304aab1f0bc5da48acb0a
秋田優斗 <yuto@akitayuutonoMacBook-Air.local>
2025-08-22 13:08:27 +0900

    create feat: new-event-container

---
+++ src/components/ui/EventCard.tsx
@@ -0,0 +1,26 @@
+import { Card, CardHeader } from "./card";
+import { Input } from "./input";
+import Image from "next/image";
+
+export default function EventCard() {
+    return (
+        <Card >
+           <CardHeader >
+            <Image
+                src="Icon-Carender.svg"
+                alt="Event Icon"
+                width={28}
+                height={28}
+                className="EventIcon"
+            />
+                <h2 >イベント名</h2>
+            </CardHeader>
+            <Input
+                type="text"
+                placeholder="イベントの名を入力:例: 歓迎会、飲み会"
+                className="bg-bg-input text-gray-700 placeholder-gray-500 focus:ring-2 focus:ring-color-mainGreen focus:border-color-mainGreen rounded-lg  w-full"
+            />
+        </Card>
+
+    );
+}
