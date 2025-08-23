736097795ec7842d69671b8faa0076cd431e87c1
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-08-24 07:20:34 +0900

    feat: pageの組み上げ

--- src/app/admin/create/page.tsx
+++ src/app/admin/create/page.tsx
@@ -1,11 +1,13 @@
 "use client";
-
 import { memo, useState } from "react";
 import EventNameCard from "@/components/ui/EventNameCard";
 import EventDurationCard from "@/components/ui/EventDurationCard";
 import EventTimeCard, { EventTimeValue } from "@/components/ui/EventTimeCard";
 import EventNeedsTime from "@/components/ui/EventNeedsTime";
 import EventMemoCard from "@/components/ui/EventMemoCard";
+import PageContent from "@/components/ui/PageContent";
+import { Header } from "@/components/ui/Header";
+import GreenButton from "@/components/ui/GreenButton";
 export default function Home() {
     const [title, setTitle] = useState("");
     const [period, setPeriod] = useState<{ start: Date | null; end: Date | null }>({
@@ -16,24 +18,31 @@ export default function Home() {
     const [time, setTime] = useState<EventTimeValue>({ option: "" });
     const [durationMin, setDurationMin] = useState(0);
     const [memo, setMemo] = useState("");
+
     
     return (
-        <div className="flex flex-col gap-4 p-6">
-            {/* イベント名 */}
-            <EventNameCard title={title} onTitleChange={setTitle} />
-            {/* 期間（開始/終了日） */}
-            <EventDurationCard
-                periodStart={period.start}
-                periodEnd={period.end}
-                onChange={setPeriod}
-            />
-            {/* 募集時間（朝/昼/夜/全日/カスタム） */}
-            <EventTimeCard value={time} onChange={setTime}>
-            </EventTimeCard>
-            {/* 所要時間 */}
-            <EventNeedsTime durationMin={durationMin} onDurationChange={setDurationMin} />
-              {/* メモらん */}
-            <EventMemoCard Memo={memo} onMemoChange={setMemo} />
-        </div>
+        <>
+          <Header link="/" title="新規イベント作成"/>
+          <PageContent bgColor="gradation">
+              {/* イベント名 */}
+              <EventNameCard title={title} onTitleChange={setTitle} />
+              {/* 期間（開始/終了日） */}
+              <EventDurationCard
+                  periodStart={period.start}
+                  periodEnd={period.end}
+                  onChange={setPeriod}
+              />
+              {/* 募集時間（朝/昼/夜/全日/カスタム） */}
+              <EventTimeCard value={time} onChange={setTime}>
+              </EventTimeCard>
+              {/* 所要時間 */}
+              <EventNeedsTime durationMin={durationMin} onDurationChange={setDurationMin} />
+                {/* メモらん */}
+              <EventMemoCard Memo={memo} onMemoChange={setMemo} />
+              <GreenButton title="条件入力完了" />
+          </PageContent>
+          
+        </>
+
     );
 }
