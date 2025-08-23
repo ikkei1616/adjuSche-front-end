b1bf6644607ed0ea41048c8b0b846d533caac247
秋田優斗 <yuto@akitayuutonoAir.lan>
2025-08-23 17:55:35 +0900

    タイトルのコンポーネント完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import EventCard from "@/components/ui/EventNameCard";
+
+import Title from "@/components/ui/Title";
 
 export default function Home() {
   return (
-    <div className="text-red-500">
-      <Button>Click me </Button>
-    </div>
+    <Title></Title>
 
   );
 }
