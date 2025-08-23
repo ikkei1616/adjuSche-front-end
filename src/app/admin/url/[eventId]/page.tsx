603c34b30b3157ebd72bec9f4dedc9f022419da5
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-08-24 01:00:26 +0900

    feat: admin/urlの実装

--- src/app/admin/url/[eventId]/page.tsx
+++ src/app/admin/url/[eventId]/page.tsx
@@ -1,3 +1,4 @@
+import GreenButton from "@/components/ui/GreenButton";
 import { Header } from "@/components/ui/Header";
 import InviteLinkCard from "@/components/ui/InviteLinkCard";
 import PageContent from "@/components/ui/PageContent";
@@ -6,11 +7,17 @@ type Props = {params: Promise<{eventId: string}>};
 
 export default async function Page({params}:Props) {
   const { eventId }  =  await params;
+  
+  // 環境変数からベースURLを取得
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+  const eventPageUrl = `${baseUrl}/guest/result/${eventId}`;
+  
   return (
     <>
       <Header link="/admin/create" title="招待用URL" />
-      <PageContent bgColor="gradation"> 
+      <PageContent bgColor="gradation" className="gap-96"> 
         <InviteLinkCard eventId={eventId}></InviteLinkCard>
+        <GreenButton href={eventPageUrl} title="イベントページを表示"></GreenButton>
       </PageContent>
     </>
   )
