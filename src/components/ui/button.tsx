bb147110d0ba74c53f2593015754407236845140
秋田優斗 <yuto@akitayuutonoAir.lan>
2025-08-23 15:10:57 +0900

    緑のコンポーネントの作成

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,11 +20,12 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        green:  "bg-mainGreen text-white border-0 rounded-lg shadow-sm w-full hover:bg-mainGreen/90 disabled:opacity-70 disabled:pointer-events-none py-3",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        default: " px-4 py-3 has-[>svg]:px-3",
+        sm: " rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
+        lg: " rounded-md px-6 has-[>svg]:px-4",
         icon: "size-9",
       },
     },
