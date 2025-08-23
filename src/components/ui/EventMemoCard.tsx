f83b1c5825fc38c594a412cc9335ce21f96665c8
秋田優斗 <yuto@akitayuutonoAir.lan>
2025-08-23 11:35:55 +0900

    メモカードを作成

---
+++ src/components/ui/EventMemoCard.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { Card, CardHeader } from "./card";
+import { Textarea } from "./textarea";
+import Image from "next/image";
+
+type EventMemoCardProps = {
+  Memo: string;
+  onMemoChange: (value: string) => void;
+};
+
+export default function EventMemoCard({ Memo, onMemoChange }: EventMemoCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <Image
+          src="Memo.svg"
+          alt="MemoIcon"
+          width={28}
+          height={28}
+          className="MemoIcon"
+        />
+        <h2>メモ</h2>
+      </CardHeader>
+      <Textarea
+        value={Memo}
+        onChange={(e) => onMemoChange(e.target.value)}
+        placeholder="イベントの詳細を入力:例:場所、持ち物"
+      />
+    </Card>
+  );
+}
